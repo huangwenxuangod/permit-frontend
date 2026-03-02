bf7c5cf6202361ee281027601f3f2ce6b3ba8ab3
huangwenxuangod <3745359@qq.com>
2026-03-02 08:42:09 +0800

    fix: 删除

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,6 @@
 import React, { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
-import { initAuthInterceptors, bootstrapAuth, quickLogin } from './services/auth'
-
-import './app.scss'
 
 function App({ children }: PropsWithChildren<any>) {
-  useLaunch(async () => {
-    initAuthInterceptors()
-    await quickLogin().catch(() => {})
-    await bootstrapAuth()
-  })
-
-  // children 是将要会渲染的页面
   return children
 }
   
